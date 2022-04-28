3a020352b7059926016c844b7448033db9799107
DanielaCeti <104537627+DanielaCeti@users.noreply.github.com>
2022-04-27 19:26:06 -0500

    main

--- Practica2.py
+++ Practica2.py
@@ -12,4 +12,33 @@ import keyboard
 import math as ma
 
 opc=0
-#image = None
+#image = Non
+
+def operaciones():
+    
+    img1 = cv2.imread('badbunny.jpg')
+    img2 = cv2.imread('danielricciardo.jpg')
+    #print (img1)
+    #print("-------------------")
+    #print (img2)
+    #print("-------------------")
+    global opc
+
+
+
+    
+raiz = Tk()
+raiz.title("Operaciones de imagenes") #Cambiar el nombre de la ventana 
+raiz.geometry("1200x480") #Configurar tamaño
+raiz.resizable(0,0)
+etiqueta_titular = ttk.Label(text="Operaciones de imagenes")
+img1vi=PhotoImage(file="badbunny.jpg")
+widget=Label(raiz,text="Imagen 1",image=img1vi).place(x=50,y=50)
+img2vi=PhotoImage(file="danielricciardo.jpg")
+widget2=Label(raiz,text="imagen 2",image=img2vi).place(x=850,y=50)
+etiqueta_titular.place(x=600, y=20)
+resultadoImagen = Label(raiz,text="Resultado")
+resultadoImagen.place(x=400,y=50)
+keyboard.on_press_key("d", lambda _:operaciones())
+
+raiz.mainloop() 
