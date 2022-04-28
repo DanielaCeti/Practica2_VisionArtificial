d1a8c089518090fe96db2ad92ad1a8120685630c
DanielaCeti <104537627+DanielaCeti@users.noreply.github.com>
2022-04-27 19:21:51 -0500

    Initial commit

---
+++ Practica2.py
@@ -0,0 +1,15 @@
+from ast import Try
+import cv2
+import numpy as np
+import PIL
+from PIL import ImageTk
+from PIL import Image
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import imutils
+import keyboard
+import math as ma
+
+opc=0
+#image = None
